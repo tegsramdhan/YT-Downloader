538dbface94f1dc5f57dfd8c526a1a56c460e5b4
tegsramdhan <ahmadteguh1999@gmail.com>
2023-03-02 13:29:37 +0700

    Add progess Bar

--- src/main.py
+++ src/main.py
@@ -3,7 +3,9 @@ from tkinter import *
 from tkinter import messagebox
 from pytube import YouTube
 from tkinter import filedialog
+from tkinter import ttk
 import sys
+import threading
 
 root = tk.Tk()
 root.title("YT Downloader")
@@ -17,7 +19,8 @@ strDirectory = StringVar()
 def paste_button_click(event):
     entryURL.delete(0, tk.END)
     entryURL.insert(0, root.clipboard_get())
-    check_URL_availability()
+    t = threading.Thread(target=check_URL_availability)
+    t.start()
 
 def on_entry_click(event):
     if entryURL.get() == "Masukkan URL":
@@ -30,12 +33,18 @@ def directory_button_click():
 
 def download_button_click():
     try :
+        loadingBar.grid(row=1, column=0, pady= 5)
+        labelPercentage.grid(row=1, column=1, padx= 5, pady= 5)
         yt = YouTube(entryURL.get(), on_progress_callback=progress_function)
         stream = yt.streams.filter(progressive=True, file_extension='mp4', res=strVideoSize.get()).first()
         strVideoName = stream.title
         stream.download( output_path= strDirectory.get(), filename= strVideoName + ".mp4")
     except Exception as e :
         print(e)
+
+def download_thread() :
+    t = threading.Thread(target=download_button_click)
+    t.start()
 
 def check_URL_availability() :
     if "tube.com" in entryURL.get() :
@@ -54,10 +63,12 @@ def check_URL_availability() :
             print("URL invalid")
 
 def progress_function(stream, chunk, bytes_remaining):
-    filesize = stream.filesize
-    current = ((filesize - bytes_remaining)/filesize)
-    percent = ('{0:.1f}').format(current*100)
-    buttonDownload.config(text=percent)
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage = (bytes_downloaded / total_size) * 100
+    print(f"Downloaded: {percentage:.2f}%")
+    loadingBar["value"] = percentage
+    labelPercentage.config(text= f"{percentage:.2f}%")
 
 
 
@@ -84,8 +95,11 @@ entryDirectory.grid(row=0, column=0)
 buttonDirectory = Button(frameDirectory, text="Browse", command=directory_button_click, width=5)
 buttonDirectory.grid(row=0, column=1)
 
-buttonDownload = Button(frameDownload, text="Download", command=download_button_click)
+buttonDownload = Button(frameDownload, text="Download", command=download_thread)
 buttonDownload.grid(row=0, column=0)
+loadingBar = ttk.Progressbar(frameDownload, orient="horizontal",length=250, mode="determinate")
+labelPercentage = Label(frameDownload, text= "0.0%")
+
 
 frameURL.pack()
 
